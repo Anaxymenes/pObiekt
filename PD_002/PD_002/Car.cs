ff036d8f3d6d0eeaa16b9033fb037f747748f862
R2D2 <acwmpl@gmail.com>
2016-10-20 23:05:13 +0200

    Praca domowa - uzupelnienie klasy Car

---
+++ PD_002/PD_002/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PD_002
+{
+    class Car
+    {
+        public string marka;
+        public int rok;
+        private string model;
+        private int iloscDrzwi;
+        private double pojemnoscSilnika;
+        public double srednieSpalanie;
+        private static int iloscSamochodow = 0;
+        
+        //praca domowa
+        private double ObliczSpalanie(double dlugoscTrasy)
+        { 
+            double spalanie = (srednieSpalanie * dlugoscTrasy)/100;
+            return spalanie;
+        }
+        public double ObliczKosztPrzejazdu(double dlugoscTrasy, double kosztPrzejazdu)
+        {
+            return ObliczSpalanie(dlugoscTrasy) * kosztPrzejazdu;
+        }
+    }
+}
---
+++ PD_002/PD_002/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PD_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string carName = "Mój samochód\n";
+
+            //Console.WriteLine(carName);
+
+            //Car car1 = new Car();
+            //car1.marka = "Ford";
+            //car1.rok = 2001;
+            //Car car2 = new Car();
+            //car2.marka = "Opel";
+            //car2.rok = 1999;
+            //Console.WriteLine("Marka: {0} Rok: {1}\n", car1.marka, car1.rok);
+            //Console.WriteLine("Marka: {0} Rok: {1}\n", car2.marka, car2.rok);
+
+            //car1 = car2;
+            //Console.WriteLine("Marka: {0} Rok: {1}\n", car1.marka, car1.rok);
+            //Console.WriteLine("Marka: {0} Rok: {1}\n", car2.marka, car2.rok);
+
+            Car car = new Car();
+            car.marka = "Ford";
+            car.rok = 2016;
+            car.srednieSpalanie = 4.8;
+            Console.Write("Podaj dystans: ");
+            double dystans = Double.Parse(Console.ReadLine());
+            Console.Write("Podaj cene za litr paliwa: ");
+            double cena = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Koszt przejazdu dla samochodu marki {0} z roku {1} \nze średnim spalaniem wynoszącym {2}, wynosi {3}", car.marka, car.rok, car.srednieSpalanie,car.ObliczKosztPrzejazdu(dystans,cena));
+
+
+            Console.ReadKey();
+        }
+    }
+}
